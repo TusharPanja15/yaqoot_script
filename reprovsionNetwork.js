c0709b12c8ef7e3f013f7c7ef205d797bbafc24b
Tushar Panja <panja.tushar15@gmail.com>
2021-12-29 11:06:40 +0530

    updated reprovisining

--- reprovsionNetwork.js
+++ reprovsionNetwork.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const request = require("request");
 const axios = require("axios");
-const { reject } = require("lodash");
 
 const con = mysql.createConnection({
     host: '10.240.37.125',
@@ -21,69 +20,110 @@ con.connect(function (err) {
 
 
 // Required Details
-const MSISDN = '831034581861' // msisdn to reprovisioned
+const MSISDNS = [] // msisdns to reprovisioned
 
+var done = []
+var failed = []
+var notFound = []
 
 async function start() {
-    let matrixData = await getMatrixData()
 
-    if (matrixData) {
-        
-        const networkData = await getNetwork(matrixData);
+    const UNDERLINE = "\x1B[4m";
+    const RESET = "\x1B[0m";
 
-        console.log("\n|+++++++ Reprovising Process Initiated ++++++++++|\n")
-        console.log("\n============== User's Matrix Data ================\n")
-        console.log("user_id \t", matrixData.user_id)
-        console.log("sim_status \t", matrixData.sim_status)
-        console.log("msisdn \t", matrixData.msisdn)
-        console.log("imsi \t", matrixData.imsi)
-        console.log("iccid \t", matrixData.iccid)
-        console.log("ki \t", matrixData.ki)
-        console.log("\n================ Network Details ================\n")
-        console.log(`EMA MSISDN \t${networkData.data.parsed[0].msisdn[0]}`)
-        console.log(`SIM IMSI \t${networkData.data.parsed[0].imsi[0]}`)
-        console.log(`Charge Type \t${networkData.data.parsed[0].hssSub[0].hssLstSub[0].gprsData[0].group[1].charge}`)
-        console.log("\nIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII\n")
-        console.log("\n◌◌◌◌◌◌◌◌◌◌◌◌◌◌ Deleting Network ◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌\n")
-        await deleteNetwork(networkData);
-        console.log("\n◌◌◌◌◌◌◌◌◌◌◌◌◌◌ Creating Network ◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌\n")
-        await createNetwork(matrixData);
-        console.log("\nIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII\n")
-        console.log("\n======= Network Details after Reprovining =======\n")
-        console.log(`EMA MSISDN \t${networkData.data.parsed[0].msisdn[0]}`)
-        console.log(`SIM IMSI \t${networkData.data.parsed[0].imsi[0]}`)
-        console.log(`Charge Type \t${networkData.data.parsed[0].hssSub[0].hssLstSub[0].gprsData[0].group[1].charge}`)
-        console.log("\n|++++++ Reprovisiong Process Completed ++++++++++|\n")
+    console.info("\n|+++++++++++ Reprovising Process Initiated +++++++++++|\n")
 
+    for (let i = 0; i < MSISDNS.length; i++) {
+
+        let matrixData = await getMatrixData(MSISDNS[i])
+
+        if (matrixData) {
+
+            console.log("\n------------- DATA for '", matrixData.msisdn, "' -------------")
+            console.log("\n\t\t" + UNDERLINE + " User's Matrix \n" + RESET)
+            console.log("user_id \t", matrixData.user_id)
+            console.log("sim_status \t", matrixData.sim_status)
+            console.log("msisdn \t", matrixData.msisdn)
+            console.log("imsi \t", matrixData.imsi)
+            console.log("iccid \t", matrixData.iccid)
+            console.log("ki \t", matrixData.ki)
+
+            let networkData = await getNetwork(matrixData);
+
+            if (networkData != 400) {
+
+                console.log("\n\t\t" + UNDERLINE + " Network Details \n" + RESET)
+                console.log(`EMA MSISDN \t${networkData.data.parsed[0].msisdn[0]}`)
+                console.log(`SIM IMSI \t${networkData.data.parsed[0].imsi[0]}`)
+                console.log(`Charge Type \t${networkData.data.parsed[0].hssSub[0].hssLstSub[0].gprsData[0].group[1].charge}`)
+
+            } else {
+                console.log("\n\t\t" + UNDERLINE + " Network Details \n" + RESET)
+                console.log(`Not found in EMA`)
+            }
+
+            console.log("\n◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌\n")
+
+            console.warn("\n\t\t" + UNDERLINE + " Deleting from Network \n" + RESET)
+            await deleteNetwork(networkData)
+            console.log("\n\t\t" + UNDERLINE + " Creating in Network \n" + RESET)
+            await createNetwork(matrixData)
+            
+            console.log("\n◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌◌\n")
+
+            networkData = await getNetwork(matrixData);
+
+            console.log("\n\t" + UNDERLINE + " Network Details after Reprovining \n" + RESET)
+            console.log(`EMA MSISDN \t${networkData.data.parsed[0].msisdn[0]}`)
+            console.log(`SIM IMSI \t${networkData.data.parsed[0].imsi[0]}`)
+            console.log(`Charge Type \t${networkData.data.parsed[0].hssSub[0].hssLstSub[0].gprsData[0].group[1].charge}`)
+
+            // await add5G(matrixData, networkData);
+
+            console.log("\n------------------------------------------------------")
+            console.log("------------------------------------------------------\n\n")
+
+        }
     }
+
+    console.log("\n|++++++++++++ Reprovisiong Process Completed +++++++++++|\n")
+
+    console.log("Reprovising done:", done,"\n")
+    console.log("Reprovising failed:", failed,"\n")
+    console.log("Data not found:", notFound,"\n")
+
     con.end()
     process.exit(0)
 }
 
 
 // get user's Matrix details
-function getMatrixData() {
+async function getMatrixData(msisdn_arr) {
     return new Promise(async (resolve, reject) => {
-        const query = `SELECT * FROM telco_provisioning.user_matrix where msisdn in (${MSISDN}) and sim_status = 'ACTIVATED'`
-        con.query(query, (err, data) => {
-            if (err) {
-                console.log(err)
-                process.exit(0)
-            } else {
-                resolve(JSON.parse(JSON.stringify(data[0])))
+        const query = `SELECT * FROM telco_provisioning.user_matrix where msisdn in (${msisdn_arr}) AND sim_status in ('ACTIVATED')`
+        con.query(query, (err, results) => {
+            if (!results.length) {
+                notFound.push(msisdn_arr)
+                resolve()
+            } else if (results.length > 0) {
+                resolve(JSON.parse(JSON.stringify(results[0])))
             }
         })
     })
 }
 
-
 // get msisdn details from network
 async function getNetwork(matrix) {
     return new Promise(async (resolve, reject) => {
-        await axios.get('https://prodokd.maanaginx.com/network-provisioning/internal/status/msisdn/' + matrix.msisdn).then(data => {
-            // console.log(data.data);
-            resolve(data.data);
-        });
+        await axios.get('https://prodokd.maanaginx.com/network-provisioning/internal/status/msisdn/' + matrix.msisdn)
+            .then(result => {
+                done.push(matrix.msisdn)
+                resolve(result.data)
+            })
+            .catch(error => {
+                failed.push(matrix.msisdn)
+                resolve(400)
+            })
     });
 }
 
@@ -94,37 +134,77 @@ async function deleteNetwork(networkData) {
         await axios.post('https://prodokd.maanaginx.com/network-provisioning/internal/subscribe/delete', {
             "msisdn": networkData.data.parsed[0].msisdn[0],
             "imsi": networkData.data.parsed[0].imsi[0]
-        }).then(data => {
-            console.log(data.data);
-            resolve(data.data);
-        });
+        })
+            .then(result => {
+                console.log(result.data);
+                resolve(result.data);
+            })
+            .catch(error => {
+                console.log(error.response.data)
+                resolve(error.response.data)
+            })
     });
 }
 
 
 // creating network for the msisdn
-async function createNetwork(matrix) {
+async function createNetwork(addingMsisdn) {
     return new Promise(async (resolve, reject) => {
 
-        if (MSISDN.length > 9) {
+        if (addingMsisdn.msisdn.length > 9 && addingMsisdn.msisdn.length == 12) {
+            
             await axios.post('https://prodokd.maanaginx.com/network-provisioning/internal/subscribe/create/data', {
-                "msisdn": matrix.msisdn,
-                "imsi": matrix.imsi,
-                "ki": matrix.ki
-            }).then(data => {
-                console.log(data.data);
-                resolve(data.data);
-            });
-        } else {
-            await axios.post('https://prodokd.maanaginx.com/network-provisioning/internal/subscribe/create', {
-                "msisdn": matrix.msisdn,
-                "imsi": matrix.imsi,
-                "ki": matrix.ki
-            }).then(data => {
-                console.log(data.data);
-                resolve(data.data);
-            });
-        }
+                "msisdn": addingMsisdn.msisdn,
+                "imsi": addingMsisdn.imsi,
+                "ki": addingMsisdn.ki
+            })
+                .then(result => {
+                    done.push(addingMsisdn.msisdn)
+                    console.log(result.data)
+                    resolve(result.data)
+                })
+                .catch(error => {
+                    failed.push(addingMsisdn.msisdn)
+                    console.log(error.response.data)
+                    resolve()
+                })
 
+        } else if (addingMsisdn.msisdn.length == 9) {
+
+            await axios.post('https://prodokd.maanaginx.com/network-provisioning/internal/subscribe/create', {
+                "msisdn": addingMsisdn.msisdn,
+                "imsi": addingMsisdn.imsi,
+                "ki": addingMsisdn.ki
+            })
+                .then(result => {
+                    done.push(addingMsisdn.msisdn)
+                    console.log(result.data)
+                    resolve(result.data)
+                })
+                .catch(error => {
+                    failed.push(addingMsisdn.msisdn)
+                    console.log(error.response.data)
+                    resolve()
+                })
+
+        } else {
+            console.log("Wrong number format!!!")
+        }
     });
 }
+
+
+//// add 5G
+// async function add5G(matrixData, networkData) {
+//     return new Promise(async (resolve, reject) => {
+//         await axios.post('https://prodokd.maanaginx.com/telco-provision/internal/telco/store/5G', {
+//             "userId": matrixData.user_id,
+//             "imsi": networkData.data.parsed[0].imsi[0],
+//             "msisdn": networkData.data.parsed[0].msisdn[0],
+//             "package_sku": "1X_esim"
+//         }).then(data => {
+//             console.log(data.data);
+//             resolve(data.data);
+//         });
+//     });
+// }
